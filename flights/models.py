41cf049608f8eec0746a24942322af0396b4ed82
lightkun10 <lifeisubw@gmail.com>
2020-10-01 08:44:11 +0700

    use wrong valid flight function

--- flights/models.py
+++ flights/models.py
@@ -28,7 +28,8 @@ class Flight(models.Model):
     # if its origin and destination is not the same, 
     # and/or its duration is positive
     def is_valid_flight(self):
-        return self.origin != self.destination and self.duration >= 0
+        # return self.origin != self.destination and self.duration >= 0
+        return self.origin != self.destination or self.duration >= 0
 
 class Passenger(models.Model):
     first = models.CharField(max_length = 64)  # first name
